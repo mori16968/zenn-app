2721f6896d5b691823019c972d97fb37db056975
中濱誠彦 <nakahamatakahiko@MBA.local>
2020-12-05 19:11:29 +0900

    test3

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -5,7 +5,7 @@
       <h2 class="subtitle">
         {{ subTitle }}
       </h2>
-      <button @click="getSomething">タスク取得だよ</button>
+      <button @click="getSomething">タスク取得だよーん</button>
       <ul v-for="post in posts" :key="post.id">
         <li style="text-align: left">{{ post.title }}</li>
       </ul>
